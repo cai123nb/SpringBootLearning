4e7707b26c8d4b7632d267f33f7ad75344273307
16525 <jianyong.cai@hand-china.com>
2017-08-07 15:28:22 +0800

    [TEST]

--- src/main/java/com/cjyong/spring/web/WebController.java
+++ src/main/java/com/cjyong/spring/web/WebController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebController {
     @GetMapping("/")
     public String hello(){
-        return "What's up Man";
+        return "What's up Man adfa";
     }
 }
